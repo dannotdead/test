d0faea236c19bdb470e32d929568f9b0ac991bd2
dannotdead <daniil.koka2000@gmail.com>
2022-08-24 11:34:00 +0700

    fix: show modal in card view, signature on card

--- src/components/CardItem/CardItem.jsx
+++ src/components/CardItem/CardItem.jsx
@@ -1,15 +1,20 @@
-import {useState} from 'react';
+import React, {useState} from 'react';
 import CloseButton from 'react-bootstrap/CloseButton';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import moment from 'moment';
 
 import './CardItem.css';
+import ModalImage from '../ModalImage';
 
 const CardItem = ({ img, store, setStore, setDisabledItems }) => {
 	const [dateTitle] = useState(
 		moment(img.timestamp).format('MMMM Do YYYY, h:mm:ss a')
 	);
+	const [show, setShow] = useState(false)
+	const [modalData, setModalData] = useState('')
+
+	const handleShow = () => setShow(true)
 
 	const deleteItem = (image) => {
 		let data = JSON.parse(localStorage.getItem('DeletedItems'))
@@ -26,14 +31,29 @@ const CardItem = ({ img, store, setStore, setDisabledItems }) => {
 	return (
 		<Col>
 			<Card>
-				<Card.Img variant='top' src={img.image} className='card-image' />
+				<Card.Img
+					variant='top'
+					src={img.image}
+					className='card-image'
+					onClick={() => {
+						handleShow()
+						setModalData(img.image)
+					}}
+				/>
 				<Card.Body>
-					<Card.Title>{dateTitle}</Card.Title>
+					<Card.Title>{img.image.split('/')[1]}</Card.Title>
+					<Card.Subtitle>Category: {img.category}</Card.Subtitle>
+					<Card.Text>Date: {dateTitle}</Card.Text>
 				</Card.Body>
 				<div className='card-close-btn'>
 					<CloseButton onClick={() => deleteItem(img.image)} />
 				</div>
 			</Card>
+			<ModalImage
+				show={show}
+				setShow={setShow}
+				modalData={modalData}
+			/>
 		</Col>
 	);
 }
