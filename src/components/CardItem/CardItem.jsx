22004ca465846601638f8cb1ad758a10e83f156c
dannotdead <daniil.koka2000@gmail.com>
2023-04-03 19:09:31 +0700

    feat: file size in cards

--- src/components/CardItem/CardItem.jsx
+++ src/components/CardItem/CardItem.jsx
@@ -1,61 +1,59 @@
-import React, {useState} from 'react';
-import CloseButton from 'react-bootstrap/CloseButton';
-import Card from 'react-bootstrap/Card';
-import Col from 'react-bootstrap/Col';
-import moment from 'moment';
+import React, { useState } from 'react'
+import CloseButton from 'react-bootstrap/CloseButton'
+import Card from 'react-bootstrap/Card'
+import Col from 'react-bootstrap/Col'
+import moment from 'moment'
 
-import './CardItem.css';
-import ModalImage from '../ModalImage';
+import ModalImage from '../ModalImage'
+
+import './CardItem.css'
 
 const CardItem = ({ img, store, setStore, setDisabledItems }) => {
-	const [dateTitle] = useState(
-		moment(img.timestamp).format('MMMM Do YYYY, h:mm:ss a')
-	);
-	const [show, setShow] = useState(false)
-	const [modalData, setModalData] = useState('')
+  const [dateTitle] = useState(moment(img.timestamp).format('MMMM Do YYYY, h:mm:ss a'))
+  const [show, setShow] = useState(false)
+  const [modalData, setModalData] = useState('')
 
-	const handleShow = () => setShow(true)
+  const handleShow = () => setShow(true)
 
-	const deleteItem = (image) => {
-		let data = JSON.parse(localStorage.getItem('DeletedItems'))
-		if (data) {
-			data = [...data, image]
-		} else {
-			data = [image]
-		}
-		setStore(store.filter(item => item.image !== image))
-		setDisabledItems((prevState) => prevState + 1)
-		localStorage.setItem('DeletedItems', JSON.stringify(data))
-	}
+  const deleteItem = (image) => {
+    let data = JSON.parse(localStorage.getItem('DeletedItems'))
+    if (data) {
+      data = [...data, image]
+    } else {
+      data = [image]
+    }
+    setStore(store.filter((item) => item.image !== image))
+    setDisabledItems((prevState) => prevState + 1)
+    localStorage.setItem('DeletedItems', JSON.stringify(data))
+  }
 
-	return (
-		<Col>
-			<Card>
-				<Card.Img
-					variant='top'
-					src={img.image}
-					className='card-image'
-					onClick={() => {
-						handleShow()
-						setModalData(img.image)
-					}}
-				/>
-				<Card.Body>
-					<Card.Title>{img.image.split('/')[1]}</Card.Title>
-					<Card.Subtitle>Category: {img.category}</Card.Subtitle>
-					<Card.Text>Date: {dateTitle}</Card.Text>
-				</Card.Body>
-				<div className='card-close-btn'>
-					<CloseButton onClick={() => deleteItem(img.image)} />
-				</div>
-			</Card>
-			<ModalImage
-				show={show}
-				setShow={setShow}
-				modalData={modalData}
-			/>
-		</Col>
-	);
+  console.log(img)
+
+  return (
+    <Col>
+      <Card>
+        <Card.Img
+          variant='top'
+          src={img.image}
+          className='card-image'
+          onClick={() => {
+            handleShow()
+            setModalData(img.image)
+          }}
+        />
+        <Card.Body>
+          <Card.Title>{img.image.split('/')[1]}</Card.Title>
+          <Card.Subtitle>Category: {img.category}</Card.Subtitle>
+          <Card.Text>Date: {dateTitle}</Card.Text>
+          <Card.Text>File size: {img.filesize}</Card.Text>
+        </Card.Body>
+        <div className='card-close-btn'>
+          <CloseButton onClick={() => deleteItem(img.image)} />
+        </div>
+      </Card>
+      <ModalImage show={show} setShow={setShow} modalData={modalData} />
+    </Col>
+  )
 }
- 
-export default CardItem;
+
+export default CardItem
